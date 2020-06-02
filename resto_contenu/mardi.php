aa41d3842f8e1f8a1e84d6debd06488d69dc0bac
ippolito21 <60745255+ippolito21@users.noreply.github.com>
2020-06-02 11:08:29 +0200

    enregi modi

---
+++ resto_contenu/mardi.php
@@ -0,0 +1,7 @@
+<?php
+include "config.php";
+include "contenu.php";
+$menu_a_afficher = $contenuSite["menus"]["mardi"];
+
+include $_dossier_template  . "page_menu_du_jour.php";
+ // $title_page = $menu_a_afficher["titre"];
